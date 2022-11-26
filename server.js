5c5769b60c2b8bc250365e433506c53606161ac7
Anshika Gusain <anshikagusain62@gmail.com>
2022-11-26 21:15:38 +0530

    changing cors

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ import Category from "./category.js";
    
 // }
 
+
 const app=express();
 app.use(express.json());
-app.use(cors({origin: "*"}));
+app.use(cors());
 
 // app.use(function (req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
